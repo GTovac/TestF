fc60552f7286fd4b5654ef8764daaf0c775aff50
GTovac <german.tovar.mt@usa.edu.co>
2022-10-14 11:26:22 -0500

    Ultimo

---
+++ DoctorsApp/src/main/java/com/usa/doctorsapp/repository/crudrepository/ClientCrudRepository.java
@@ -0,0 +1,9 @@
+package com.usa.doctorsapp.repository.crudrepository;
+
+
+import com.usa.doctorsapp.model.Client;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientCrudRepository extends CrudRepository<Client,Integer> {
+
+}
